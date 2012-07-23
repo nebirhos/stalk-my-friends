64382ba9025fec4f245aeac99c024f60f8804f9e
nebirhos <nebirhos@aol.com>
2012-07-23 11:26:00 +0200

    Migrate to postgis complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :address, :latitude, :longitude, :position, :name
+  attr_accessible :address, :position, :name
 
   self.rgeo_factory_generator = RGeo::Geographic.simple_mercator_factory
 
@@ -7,5 +7,9 @@ class User < ActiveRecord::Base
     radius = radius.to_i*1000
     User.where( "ST_Distance(position, ?, false) <= ? ",
                 position, radius ).where("id <> ?", id)
+  end
+
+  def as_json(options={})
+    super.as_json(options).merge({:latitude => position.y, :longitude => position.x})
   end
 end
